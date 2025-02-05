ef01bf3757ecb16231b017b9cc11954d1d8437d0
Angel Coronel <angelcoronel15@gmail.com>
2025-02-05 17:50:57 -0500

    Updated experience content

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,6 +6,8 @@ import { LuGraduationCap } from "react-icons/lu";
 import aiComputerSecurityProject from "@/resources/aiComputerSecurityProject.png";
 import minerfitproject from "@/resources/minerfit.png";
 import whip from "@/resources/whip.png";
+import mouse from "@/resources/micromouse.png";
+import cars from "@/resources/carClassification.png";
 
 export const links = [
     {
@@ -52,7 +54,7 @@ export const experiencesData = [
         date: "2023 - 2024",
     },
     {
-        title: "Software Engineer Intern - Northrop Grumman Corp.",
+        title: "Software Engineer Intern - Northrop Grumman Corp. - Secret Security Clearance",
         location: "Colorado Springs, CO",
         description:
             "As a return intern in a different team, I built a Kubernetes-based full-stack application to manage and visualize logging information.",
@@ -68,7 +70,7 @@ export const experiencesData = [
         date: "December 2023",
     },
     {
-        title: "Software Engineer Intern - Northrop Grumman Corp.",
+        title: "Software Engineer Intern - Northrop Grumman Corp. - Secret Security Clearance",
         location: "Colorado Springs, CO",
         description:
             "I added features and debugged issues on a radar that overlooks the northern hemisphere relative to the USA. My features included front-end and back-end functionalities written in Java.",
@@ -100,6 +102,14 @@ export const experiencesData = [
         date: "2022 - 2023",
     },
     {
+        title: "Tech Exchange Program - Google",
+        location: "El Paso, TX",
+        description:
+            "Completed a semester of courses including Applied Data Structures (taught in python), Product Management, and Careers in Tech taught by Google Engineers through a virtual academic program hosted by Google and my HSI (UTEP).",
+        icon: React.createElement(LuGraduationCap),
+        date: "Spring 2022",
+    },
+    {
         title: "Research Assistant - The University of Texas at El Paso",
         location: "El Paso, TX",
         description:
@@ -113,23 +123,37 @@ export const projectsData = [
     {
         title: "Computer Security Semantic Chatbox",
         description:
-            "I worked as a full-stack developer on this practicum project with 5 other devs. Users can ask computer security related questions and AI will give a response on issues and solutions.",
+            "Users can ask computer security related questions and a custom AI will give a response on issues and solutions. I worked as a full-stack developer on this practicum project with 5 other devs. Answers are based on curated embeddings built from Common Weakness Enumeration (CWE) and MITRE ATT&CK knowledge bases. Google's BERT and OpenAI's models are used for both creation and comparisons of databases embeddings as well as the user query.",
         tags: ["Python", "Gradio", "Flask", "WeaviateDB", "OpenAI", "Bert AI"],
         imageUrl: aiComputerSecurityProject,
     },
     {
         title: "MinerFIT",
         description:
-            "Fitness web app for UTEP students. I was a full-stack developer in small team. It has features like authorization/login, custom workout creation, exercise tracking services, nutrition tracking and user-led community forums.",
+            "Fitness web app for UTEP students. I was a full-stack developer in small team of 6 devs. This website built from scratch has features like authorization/login, custom workout creation, exercise tracking services, nutrition tracking and user-led community forums. We used firebase database for authentication and user data storage.",
         tags: ["Svelte", "Firebase", "HTML", "CSS", "Javascript"],
         imageUrl: minerfitproject,
     },
     {
         title: "WHIP - Women's Health Insights Platform",
         description:
-            "A public web app for quick analytics on women's health. Connect's a wearable such as an Apple watch or Oura ring to an analytical AI to track women's health activity and custom dashboard insights.",
+            "A public web app for quick analytics on women's health. Connect's a person wearable (i.e. Apple watch or Oura ring) info to an analytical AI through an API to track women's health activity and display on custom dashboard insights. This project was part of a 3-day \"Hack-a-thon\" competition, namely Borderland AI Hackathon, where my 2-person team won 1st place with a $2k price after building and showcasing the project.",
         tags: ["Bubble.io", "OpenAI", "Apple API", "Oura API", "MySQL"],
         imageUrl: whip,
+    },
+    {
+        title: "EZ-PARK - Convolutional Neural Networks Car Detection Project",
+        description:
+            "Implemented a convolutional neural network (CNN) and applied it to parked cars & parking space images to classify the data as being occupied or empty. We used a database from Kaggle for training, cleaned the data & implemented multiple layers to the convolutional neural network for classication consistenting of 2d convolution layers, max pooling, and dense layers reaching an accuracy of 92%. This was a semester-long school project with 2 other grad students worth 40% of the course grade.",
+        tags: ["AI", "ML", "Neural Network", "Python", "Keras", "MatPlotLib", "Numpy", "You Only Look Once ML Algorithm"],
+        imageUrl: cars,
+    },
+    {
+        title: "MicroMouse - Autonomous Robot Competition",
+        description:
+            "As a freshman in Computer Science, I teamed up with 3 Electrical Engineering students to build and program a robot from scratch. The robot’s aim was to be placed in a 10ft. by 10ft maze and find the center of the maze as fast as possible with no outside help. The robot had DC motors, Arduino Nano microcontroller, a battery, Bluetooth, LED proximity sensors, and encoders sottered together then programmed for the competition. We won 1st place in the local IEEE chapter of our university.",
+        tags: ["C++", "Arduino Nano", "Flood Fill Algorithm", "L293D DC Motor Drive", "MPU6050 IMU", "IR LED Proximity Sensors"],
+        imageUrl: mouse,
     },
 ] as const;
 
