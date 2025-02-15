ec7ef66af891de3c90c258d871e047d6313ad88e
Angel Coronel <angelcoronel15@gmail.com>
2025-02-14 20:33:11 -0500

    Fixed buy me a coffee link

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -101,12 +101,14 @@ export default function Intro() {
                 </a>
 
                 <a
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                    href="https://www.linkedin.com/in/an-cor"
+                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                    href="https://buymeacoffee.com/angelcoronel"
                     target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Buy me a coffee"
                 >
                     Buy me a coffee!{" "}
-                    <AiOutlineCoffee className="opacity-70 group-hover:translate-x-1 transition" />
+                    <AiOutlineCoffee className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
 
                 <a
